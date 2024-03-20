e1cd19250b78c1afa4be1bb301811252752c0e22
JayvonK <jaykhuth@gmail.com>
2024-03-20 13:01:05 -0700

    part 1 done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,12 +26,12 @@ export default function Home() {
 
   const handleSubmit = () => {
     // Putting our user data inside of an object so we can put it in our Post fetch
-    let userDatea = {
+    let userData = {
       username: username,
       password: password
     }
 
-    if(switchBool) {
+    if (switchBool) {
       // Create account logic in here
 
     } else {
@@ -44,24 +44,24 @@ export default function Home() {
     <div className="grid grid-flow-row justify-center mt-20">
       <div className="bg bg-slate-500 min-w-96 p-8 rounded-lg">
         <h1 className="text-3xl">{switchBool ? 'Create Account' : 'Login'}</h1>
-      <form className="flex max-w-md flex-col gap-4">
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="username" value="username" />
-        </div>
-        <TextInput id="username" type="text" placeholder="enter username" required  onChange={(e) => setUsername(e.target.value)}/>
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="password1" value="Your password" />
-        </div>
-        <TextInput id="password1" type="password" required onChange={(e) => setPassword(e.target.value)}/>
-      </div>
-      <div className="flex items-center gap-2">
-        <Button onClick={handleSwitch} color="light">{switchBool ? 'Already have an account?' : 'Sign up'}</Button>
-      </div>
-      <Button onClick={handleSubmit} type="submit">Submit</Button>
-    </form>
+        <form className="flex max-w-md flex-col gap-4">
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="username" value="username" />
+            </div>
+            <TextInput id="username" type="text" placeholder="enter username" required onChange={(e) => setUsername(e.target.value)} />
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="password1" value="Your password" />
+            </div>
+            <TextInput id="password1" type="password" required onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <div className="flex items-center gap-2">
+            <Button onClick={handleSwitch} color="light">{switchBool ? 'Already have an account?' : 'Sign up'}</Button>
+          </div>
+          <Button onClick={handleSubmit} type="submit">Submit</Button>
+        </form>
 
       </div>
     </div>
